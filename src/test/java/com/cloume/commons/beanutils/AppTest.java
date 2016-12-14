70f825cbfad3843113067febb0a6678fdd5712f6
GANG XU <GANG XU>
2016-12-14 21:13:01 +0800

    补充了测试用例
    Merge branch 'master' of https://github.com/HQIT/commons-beanutils
    
    # Conflicts:
    #	.gitignore

--- src/test/java/com/cloume/commons/beanutils/AppTest.java
+++ src/test/java/com/cloume/commons/beanutils/AppTest.java
@@ -1,5 +1,8 @@
 package com.cloume.commons.beanutils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -10,6 +13,18 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+	class InnerClass {
+		String someValue;
+	}
+	
+	class ObjectToUpdate {
+		int intValue;
+		long longValue;
+		String stringValue;
+		Object[] arrayValue;
+		InnerClass classValue;
+	}
+	
     /**
      * Create the test case
      *
@@ -31,7 +46,24 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testUpdater()
+    {
+    	InnerClass iclass = new InnerClass();
+    	Map<String, Object> from = new HashMap<String, Object>();
+    	from.put("intValue", 100);
+    	from.put("longValue", 200L);
+    	from.put("stringValue", "this is string");
+    	from.put("classValue", iclass);
+    	
+    	ObjectToUpdate updated = Updater.wrap(new ObjectToUpdate()).update(from);
+    	assertNull(updated.arrayValue);
+    	assertEquals(100, updated.intValue);
+    	assertEquals(200L, updated.longValue);
+    	assertEquals("this is string", updated.stringValue);
+    	assertEquals(iclass, updated.classValue);
+    }
+    
+    public void testUpdaterWithConverter()
     {
         assertTrue( true );
     }
